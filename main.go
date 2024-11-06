f8f41431884aef4bd5037432aa9e4b50d7e5d2bf
fridalif <fridalif>
2024-11-06 10:49:50 +0300

    Асинхронный вызов хендлеров для сниффинга

--- main.go
+++ main.go
@@ -5,21 +5,37 @@ import (
 	"fmt"
 	"log"
 	"github.com/google/gopacket/pcap"
+	"sync"
 )
+
+func sniffer(iface Interface, wg *sync.WaitGroup) {
+	defer wg.Done()
+}
 
 func main() {
 
 	//Инициализация логирования
 	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	defer file.Close()
 	if err != nil {
-    	log.Fatalln("ERROR:Failed to open log file:", err)
+    	log.Fatalln("ERROR:Ошибка при открытии файла:", err)
 	}
 	log.SetOutput(file)
 
 	//Получение сетевых интерфейсов
 	devices, err := pcap.FindAllDevs()
     if err != nil {
-        log.Fatalln("ERROR:Failed to get devices:", err)
+        log.Fatalln("ERROR:Ошибка получения сетевых интерфейсов:", err)
     }
-	
+	wg := new(sync.WaitGroup)
+
+	//Запуск прослушивания интерфейсов
+	for _, device := range devices {
+		wg.Add(1)
+		go sniffer(device, wg)
+	}
+	wg.Wait()
+
+	//Закрытие логирования
+	log.Println("Info: Программа завершена")
 }
