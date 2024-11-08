1f75b58fcfbfeade393d62a4a377b6e36e3a7d69
fridalif <fridalif>
2024-11-08 17:20:12 +0300

    Установлена библиотека gin, начат файл main

---
+++ webserver/README.md
@@ -0,0 +1,8 @@
+# web interface
+
+#### Intsallation
+
+```Shell
+go mod init webinterface
+go get -u github.com/gin-gonic/gin
+```
---
+++ webserver/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"github.com/gin-gonic/gin"
+)
+  
+var router *gin.Engine
+
+func main() {
+	return
+}
