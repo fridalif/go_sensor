979f701f21a7ae5ce40b9669c24025aed2531259
fridalif <fridalif>
2024-11-10 00:49:50 +0300

    Сделана начальная загрузка данных по websocket

--- webserver/static/js/script.js
+++ webserver/static/js/script.js
@@ -3,9 +3,7 @@ function alerting(){
 }
 
 
-function onMessage(event) {
-    const alerts = JSON.parse(event.data);
-    console.log(alerts);
+function setAlerts(alerts) {
     let alertsTable = document.getElementById("alertsTable");
     let newElement = document.createElement("div");
     newElement.className = "rulesBlockContentRow";
@@ -50,4 +48,70 @@ function onMessage(event) {
     newElement.appendChild(checksum);
     newElement.appendChild(ttl);
     alertsTable.appendChild(newElement);
+}
+
+function setComputers(computers) {
+    let computersTable = document.getElementById("computersTable");
+    let newElement = document.createElement("div");
+    newElement.className = "computersBlockContentRow";
+    let computer = document.createElement("div");
+    computer.className = "computersCell";
+    computer.innerHTML = computers.Name;
+    let address = document.createElement("div");
+    address.className = "computersCell";
+    address.innerHTML = computers.Address;
+    newElement.appendChild(computer);
+    newElement.appendChild(address);
+    computersTable.appendChild(newElement);
+}
+
+function setRules(rules) {
+    let rulesTable = document.getElementById("rulesTable");
+    let newElement = document.createElement("div");
+    newElement.className = "rulesBlockContentRow";
+    let deleteBut = document.createElement("div");
+    deleteBut.className = "deleteButton";
+    deleteBut.innerHTML = "-";
+    newElement.appendChild(deleteBut);
+    let layer = document.createElement("div");
+    layer.className = "rulesCell";
+    layer.innerHTML = rules.Netlayer.Name;
+    let srcip = document.createElement("div");
+    srcip.className = "rulesCell";
+    srcip.innerHTML = rules.SrcIp;
+    let dstip = document.createElement("div");
+    dstip.className = "rulesCell";
+    dstip.innerHTML = rules.DstIp;
+    let contains = document.createElement("div");
+    contains.className = "rulesCell";
+    contains.innerHTML = rules.PayloadContains;
+    let srcport = document.createElement("div");
+    srcport.className = "rulesCell";
+    srcport.innerHTML = rules.SrcPort;
+    let dstport = document.createElement("div");
+    dstport.className = "rulesCell";
+    dstport.innerHTML = rules.DstPort;
+    let checksum = document.createElement("div");
+    checksum.className = "rulesCell";
+    checksum.innerHTML = rules.Checksum;
+    newElement.appendChild(layer);
+    newElement.appendChild(srcip);
+    newElement.appendChild(dstip);
+    newElement.appendChild(contains);
+    newElement.appendChild(srcport);
+    newElement.appendChild(dstport);
+    newElement.appendChild(checksum);
+    rulesTable.appendChild(newElement);
+}
+function onMessage(event) {
+    const response = JSON.parse(event.data);
+    if (response.table_name == "alerts") {
+        setAlerts(response.data);
+    }
+    if (response.table_name == "computers") {
+        setComputers(response.data);
+    }
+    if (response.table_name == "rules") {
+        setRules(response.data);
+    }
 }
--- webserver/views/views.go
+++ webserver/views/views.go
@@ -18,22 +18,90 @@ var upgrader = websocket.Upgrader{
     },
 }
 
+type AlertMessage struct {
+    TableName string `json:"table_name"`
+    Data      models.Alert  `json:"data"`       
+}
+
+type ComputerMessage struct {
+    TableName string `json:"table_name"`
+    Data      models.IncludedComputer  `json:"data"`       
+}
+
+type RuleMessage struct {
+    TableName string `json:"table_name"`
+    Data      models.Rule  `json:"data"`
+}
+
+//var alertsChanel = make(chan models.Alert)
+
+var clients = make([]*websocket.Conn, 0)
+func closeConn(conn *websocket.Conn) {
+    conn.Close()
+    for i, c := range clients {
+        if c == conn {
+            clients = append(clients[:i], clients[i+1:]...)
+            break
+        }
+    }
+}
 func WSHandler(c *gin.Context, db *gorm.DB) {
 	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
         log.Println("Ошибка при установлении WebSocket-соединения:", err)
         return
     }
-    defer conn.Close()
+    clients = append(clients, conn)
+
+    defer closeConn(conn)
+    var computers []models.IncludedComputer
+    if err := db.Find(&computers).Error; err != nil {
+        log.Println("Ошибка при получении записей:", err)
+        return
+    }
+
+    for _, computer := range computers {
+        message := ComputerMessage{
+            TableName: "computers",
+            Data:      computer,
+        }
+        if err := conn.WriteJSON(message); err != nil {
+            log.Println("Ошибка при отправке сообщения:", err)
+            return
+        }
+    }
+    
+    var rules []models.Rule
+    if err := db.Preload("Netlayer").Find(&rules).Error; err != nil {
+        log.Println("Ошибка при получении записей:", err)
+        return
+    }
+
+    for _, rule := range rules {
+        message := RuleMessage{
+            TableName: "rules",
+            Data:      rule,
+        }
+        if err := conn.WriteJSON(message); err != nil {
+            log.Println("Ошибка при отправке сообщения:", err)
+            return
+        }
+    }
 
     var alerts []models.Alert
+    
+    
     if err := db.Preload("Computer").Preload("Rule").Preload("Rule.Netlayer").Order("timestamp desc").Find(&alerts).Error; err != nil {
         log.Println("Ошибка при получении записей:", err)
         return
     }
 
     for _, alert := range alerts {
-        if err := conn.WriteJSON(alert); err != nil {
+        message := AlertMessage{
+            TableName: "alerts",
+            Data:      alert,
+        }
+        if err := conn.WriteJSON(message); err != nil {
             log.Println("Ошибка при отправке сообщения:", err)
             return
         }
